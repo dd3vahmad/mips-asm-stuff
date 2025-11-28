18d3383ab31ed062af4b177d2e564215595b58ef
Ahmad <rabiu2993@gmail.com>
2025-11-28 23:43:18 +0100

    the perfect one

--- mips.asm
+++ mips.asm
@@ -8,23 +8,18 @@
     fibOutput: .asciiz "The value of the: "
     fibOutput2: .asciiz "th Fibonnaci value is: "
     fibValue: .word 0
-    TRUE = 1
-    FALSE = 0
-    INT_PRINT = 1
-    STRING_PRINT = 4
-    INT_READ = 5
 
 .text
 .globl main
 .ent main
 main:
     # print prompt
-    li $v0, STRING_PRINT
+    li $v0, 4
     la $a0, fibInput
     syscall     # syscall: print string
 
     # read integer n into $v0
-    li $v0, INT_READ
+    li $v0, 5
     syscall     # syscall: read int
 
     # save n to fibValue
@@ -36,19 +31,19 @@ main:
 
     # CHECKS: DO NOT CHANGE
     move $s0, $v0
-    li $v0, STRING_PRINT
+    li $v0, 4
     la $a0, newLine
     syscall
-    li $v0, STRING_PRINT
+    li $v0, 4
     la $a0, fibOutput
     syscall
-    li $v0, INT_PRINT
+    li $v0, 1
     lw $a0, fibValue
     syscall
-    li $v0, STRING_PRINT
+    li $v0, 4
     la $a0, fibOutput2
     syscall
-    li $v0, INT_PRINT
+    li $v0, 1
     move $a0, $s0
     syscall
     li $v0, 10
@@ -62,29 +57,28 @@ main:
 .globl fib
 .ent fib
 fib:
-    subu $sp, $sp, 8    # allocate stack space
+    subu $sp, $sp, 12   # allocate stack space (increased to 12)
     sw $ra, ($sp)       # save return address
     sw $a0, 4($sp)      # save argument
-
+    move $v0, $a0       # set v0 for base case
     # check for base cases
-    li $t0, TRUE
+    li $t0, 1
     ble $a0, $t0, fibDone
-
     # get fib(n-1)
-    sub $a0, $a0, 1
+    li $t1, 1
+    sub $a0, $a0, $t1
     jal fib
-    move $t0, $v0       # save fib(n-1)
-
+    sw $v0, 8($sp)      # FIXED: save fib(n-1) on stack instead of $t0
     # get fib(n-2)
     lw $a0, 4($sp)
-    sub $a0, $a0, 2
+    li $t1, 2
+    sub $a0, $a0, $t1
     jal fib
-
     # fib(n-1) + fib(n-2)
+    lw $t0, 8($sp)      # FIXED: restore fib(n-1) from stack
     add $v0, $v0, $t0
-
 fibDone:
     lw $ra, ($sp)       # restore ra
-    addu $sp, $sp, 8    # deallocate stack
+    addu $sp, $sp, 12   # deallocate stack (changed to 12)
     jr $ra
 .end fib
